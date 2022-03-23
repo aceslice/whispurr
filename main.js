31fc44e8e0fce789b5fb82bdf3a98a522a10422a
Gideon Appau <gideonad.codez@gmail.com>
2022-03-23 08:41:04 -0700

    Initial commit

--- main.js
+++ main.js
@@ -3,22 +3,38 @@ let container = document.querySelector(".chat")
 
 sendBtn.addEventListener("click", () => {
     let userText = document.querySelector(".user-input").value;
+    if (userText.length === 0 || userText === " ") {
+        doNothing();
+    }
     let user = document.createElement("div");
     let userInput = document.createElement("p");
     let input = document.createTextNode(userText);
     user.classList.add("user");
     userInput.append(input);
     user.append(userInput);
-    container.append(user)
-    userText = " ";
+    container.append(user);
     setTimeout(() => {
+        let botText;
         let userText = document.querySelector(".user-input").value;
+        let greetingResponse = ["I am Botty. May I know your name?", "Howdy mate", "Hey, how has it been?"];
+        let greeting = ["hi", "hello", "howdy", "hello there", "hi there", "hey", "hey there"];
+        for (let i = 0; i < greeting.length; i++) {
+           if(userText === greeting[i]){
+            let randomGreeting = (greetingResponse[Math.floor(Math.random() * greetingResponse.length)]);
+            botText = randomGreeting;
+            break;
+           }
+            else{
+                botText = userText;
+            }
+        }
         let user = document.createElement("div");
         let userInput = document.createElement("p");
-        let input = document.createTextNode(userText);
+        let input = document.createTextNode(botText);
         user.classList.add("bot");
         userInput.append(input);
         user.append(userInput);
-        container.append(user)
+        container.append(user);
     }, 2000);
+
 });
