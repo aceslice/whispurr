b4cf498dc34aa79e74f013b1517838fc95d67e1c
Gideon Appau <gideonad.codez@gmail.com>
2022-03-24 09:04:23 -0700

    Initial Commit

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 let sendBtn = document.querySelector(".send");
 let container = document.querySelector(".chat")
 
-sendBtn.addEventListener("click", () => {
+sendBtn.addEventListener("click", (e) => {
+    e.preventDefault();
     let userText = document.querySelector(".user-input").value;
     if (userText.length === 0 || userText === " ") {
         doNothing();
@@ -16,16 +17,29 @@ sendBtn.addEventListener("click", () => {
     setTimeout(() => {
         let botText;
         let userText = document.querySelector(".user-input").value;
-        let greetingResponse = ["I am Botty. May I know your name?", "Howdy mate", "Hey, how has it been?"];
+        let greetingResponse = ["Hello, I am Botty. May I know your name?", "Howdy mate", "Hey, how has it been?"];
         let greeting = ["hi", "hello", "howdy", "hello there", "hi there", "hey", "hey there"];
+        let chatUp = ["how are you?", "how has it been?", "how do you do?", "whats up?", "how are you doing?"];
+        let chatUpResponse = ["I am doing great", "Never better", "I can not find the right word to describe how my day is going", "Great", "Cool"];
         for (let i = 0; i < greeting.length; i++) {
-           if(userText === greeting[i]){
-            let randomGreeting = (greetingResponse[Math.floor(Math.random() * greetingResponse.length)]);
-            botText = randomGreeting;
-            break;
-           }
-            else{
+
+            if (userText === greeting[i]) {
+                let randomGreeting = (greetingResponse[Math.floor(Math.random() * greetingResponse.length)]);
+                botText = randomGreeting;
+                break;
+            }
+            else {
                 botText = userText;
+            }
+            for (let j = 0; j < chatUp.length; j++) {
+
+                if (userText === chatUp[j]) {
+                    botText = chatUpResponse[Math.floor(Math.random() * chatUpResponse.length)];
+                    break;
+                } else {
+                    botText = userText;
+                }
+
             }
         }
         let user = document.createElement("div");
